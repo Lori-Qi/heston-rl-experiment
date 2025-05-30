a2e94c3d50bc01acd9c839111ae19dc1029beef6
Lori Qi <lori.qi27@outlook.com>
2025-05-30 21:20:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from training_pipeline import run_heston_rl_experiment
 if __name__ == "__main__":
    
     results_df = run_heston_rl_experiment(
-        num_simulation_worlds=2, 
+        num_simulation_worlds=100, 
         agent_max_training_eps=1500, 
         wgan_training_iterations=2000, 
         wgan_sim_paths_count=1000 
